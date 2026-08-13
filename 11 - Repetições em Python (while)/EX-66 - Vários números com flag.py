d634987025e1d13eb4b87a52f5f3c76ec17fc265
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-13 08:26:48 -0300

    Exercicio 66 Finalizado!

--- 11 - Repetições em Python (while)/EX-66 - Vários números com flag.py
+++ 11 - Repetições em Python (while)/EX-66 - Vários números com flag.py
@@ -1,1 +1,14 @@
 # Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999. No final, mostre quantos números foram digitados e qual foi a soma entre eles (utilizando break para desconsiderar o flag).
+
+soma = cont = num = 0
+
+while True:
+    num = int(input("Digite um número [999 para PARAR]: "))
+
+    if num == 999:
+        break
+    else:
+        soma += num
+        cont += 1
+
+print(f"\nNumeros Digitados: {cont}\nSoma: {soma}\n")
