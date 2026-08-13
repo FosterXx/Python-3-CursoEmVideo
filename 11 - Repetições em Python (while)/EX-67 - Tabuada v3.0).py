592ee022be8c5abe04faba4ade76cc8f1bf5860b
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-13 08:34:11 -0300

    Exercicio 67 Finalizado!

--- 11 - Repetições em Python (while)/EX-67 - Tabuada v3.0).py
+++ 11 - Repetições em Python (while)/EX-67 - Tabuada v3.0).py
@@ -1,1 +1,12 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+
+while True:
+    tabuada = int(input("\nDigite a Tabuada: "))
+
+    if tabuada < 0:
+        break
+    else:
+        for c in range(1,11):
+            print(f"{tabuada} x {c} = {tabuada * c}") 
+
+print("\nPrograma Finalizado\n")
