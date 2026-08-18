3b1c6b7d4eaea60137ada6f84e40504f6c54395b
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-17 21:58:27 -0300

    Desenvolvendo e absorvendo o conteudo da Aula 16 - Tuplas!

---
+++ 13 - Tuplas em Python/Aula 16 – Tuplas.py
@@ -0,0 +1,43 @@
+###############################################################
+
+# Tuplas = Guarda até 4 valores;
+
+# 4 valores em 4 indices = ( 0 , 1 , 2 , 3 )
+
+# lanche = ('burguer','suco','pizza','pudim')
+
+# # Fatiamento;
+# print(lanche[0:3]) # ('burguer','suco','pizza')
+# print(lanche[-1]) # ('pudim')
+# print(lanche[1:]) # ('suco','pizza','pudim')
+# print(lanche[-3]) # ('suco')
+
+# # Ler os elementos da tupla;
+# len(lanche) # 4 elementos
+
+# # Percorrer todos os elementos da tupla;
+# for c in lanche:
+#     print(c)
+
+###############################################################
+
+# AS TUPLAS SÃO IMUTÁVEIS! Não tem como trocar o PUDIM por SORVETE!
+
+lanche = ('Hamburguer','Suco','Pizza','Pudim','Batata Frita')
+
+# for comida in lanche:
+#     print(f"Eu vou comer {comida}!")
+
+# Eu vou comer Hamburguer!
+# Eu vou comer Suco!
+# Eu vou comer Pizza!
+# Eu vou comer Pudim!
+
+# for cont in range(0, len(lanche)):
+    # print(lanche[cont])
+
+# Hamburguer cont = 0
+# Suco cont = 1
+# Pizza cont = 2
+# Pudim cont = 3
+# Batata Frita cont = 4
