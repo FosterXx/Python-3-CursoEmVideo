251c84a68f83d8fc825ac4e8d6e8fa0d386fdf23
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-19 15:53:31 -0300

    EX-83 Finalizado!

--- 14 - Listas em Python/EX-83 - Validando expressões matemáticas.py
+++ 14 - Listas em Python/EX-83 - Validando expressões matemáticas.py
@@ -1,2 +1,41 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
+# expressao = []
+# expressao.append(str(input("Digite a Expressão: ")).strip().upper())
+
+# contAbre = 0
+# contFecha = 0
+
+# for v in expressao:
+#     if v == '(':
+#         contAbre += 1
+#     elif v == ')':
+#         contFecha += 1
+
+# if contAbre == contFecha:
+#     print("Expressão Valida!")
+# else:
+#     print("Expressão Inválida!")
+
+###############################################################
+
+# Professor;
+
+expr = str(input("Digite a expressão: ")).strip()
+
+pilha = []
+
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+
+if len(pilha) == 0:
+    print("Expressão Válida!")
+else:
+    print("Expressão Inválida!")
