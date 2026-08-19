4ec9ce25be07438eae3a412a7f09e1f42ac793fc
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-19 08:24:39 -0300

    EX-77 e Modulo Tuplas em Python Finalizados!

--- 13 - Tuplas em Python/EX-77 - Contando vogais em Tupla.py
+++ 13 - Tuplas em Python/EX-77 - Contando vogais em Tupla.py
@@ -1,1 +1,20 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+
+palavras = (
+    'Correr',
+    'Voar',
+    'Nadar',
+    'Viver',
+    'Morrer',
+    'Assistir',
+    'Faculdade',
+)
+
+for p in palavras:
+    print(f"\nA palavra '{p.upper()}' possui as vogais:", end=' ')
+
+    for letra in p:
+        if letra in "AaEeIiOoUu":
+            print(f"{(letra).lower()}", end=' ')
+
+print("\n\nPrograma Finalizado!\n")
