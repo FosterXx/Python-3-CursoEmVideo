474099836ab6822c34c3fbd93dc0759c4b442fdf
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-19 15:29:02 -0300

    EX-82 Finalizado!

--- 14 - Listas em Python/EX-82 - Dividindo valores em várias listas.py
+++ 14 - Listas em Python/EX-82 - Dividindo valores em várias listas.py
@@ -1,2 +1,22 @@
 # Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, exiba o conteúdo das três listas geradas.
 
+valores = []
+pares = []
+impares = []
+
+while True:
+    valor = float(input("Digite um valor: "))
+    valores.append(valor)
+
+    if valor % 2 == 0:
+        pares.append(valor)
+    else:
+        impares.append(valor)
+
+    next = str(input("\nDeseja Continuar[Sim/Não]: ")).strip()[0]
+    if next in 'Nn' or next not in 'NnSs':
+        break
+
+print(f"\nValores: {valores}")
+print(f"Pares: {pares}")
+print(f"Impares: {impares}\n")
