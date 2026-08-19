d3bc0ad66269da2506aeece2e84c872232612ab2
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-19 15:20:27 -0300

    EX-81 Finalizado!

--- 14 - Listas em Python/EX-81 - Extraindo dados de uma Lista.py
+++ 14 - Listas em Python/EX-81 - Extraindo dados de uma Lista.py
@@ -3,3 +3,22 @@
 # A lista de valores, ordenada de forma decrescente.
 # Se o valor 5 foi digitado e está ou não na lista.
 
+valores = []
+
+while True:
+    valor = float(input("\nDigite o valor: "))
+    valores.append(valor)
+
+    next = str(input("\nDeseja continuar[Sim/Não]: ")).strip().upper()[0]
+    if next in 'Nn' or next not in 'SsNn':
+        break
+       
+print(f"\nNumeros Registrados: {len(valores)}")
+
+valores.sort(reverse=True)
+print(f"Lista Ordenada Decrescente: {valores}")
+
+if 5 in valores:
+    print("O número 5 foi digitado e está na lista!\n")
+else:
+    print("O número 5 não foi digitado e não está na lista!\n")
