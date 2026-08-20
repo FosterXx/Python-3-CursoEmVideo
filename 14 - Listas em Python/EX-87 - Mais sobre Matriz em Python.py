f96076fe8d472dc02d5d6b8f327e185c75baae07
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-20 16:45:20 -0300

    EX-87 Finalizado!

--- 14 - Listas em Python/EX-87 - Mais sobre Matriz em Python.py
+++ 14 - Listas em Python/EX-87 - Mais sobre Matriz em Python.py
@@ -3,3 +3,33 @@
 # A soma dos valores da terceira coluna.
 # O maior valor da segunda linha.
 
+matriz = []
+linha = []
+pares = somaColuna = maiorLinha = 0
+
+for l in range(0,3):
+    for c in range(0,3):
+        linha.append(int(input(f"Digite o valor da Linha[{l}] - Coluna [{c}]: ")))
+
+    matriz.append(linha[:])
+    linha.clear()
+
+print("")
+
+for l in range(0,3): # Percorre Todas as Linhas;
+    for c in range(0,3): #Percorre Todos os Valores na Linha = Colunas;
+
+        if matriz[l][c] % 2 == 0:
+            pares += matriz[l][c] # Soma os Pares da Matriz;
+
+        if c == 2:
+            somaColuna += matriz[l][c] # Soma a Terceira Coluna = Coluna[2];
+
+        if l == 1 and matriz[l][c] > maiorLinha: 
+            maiorLinha = matriz[l][c] # Registra o Maior Valor da Segunda Linha = Linha[1];
+
+        print(f"[{matriz[l][c]:^5}]", end='') # Imprimi a Matriz;
+    print()
+
+
+print(f"\nSoma dos Pares: {pares}\nSoma Coluna[2]: {somaColuna}\nMaior Valor Linha[1]: {maiorLinha}\n") # Imprimi as Somas e o Maior Valor;
