5a8e2c401eca25d4a81660d9360f56aed24b99f9
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-20 09:16:31 -0300

    EX-84 Finalizado!

--- 14 - Listas em Python/EX-84 - Lista composta e análise de dados.py
+++ 14 - Listas em Python/EX-84 - Lista composta e análise de dados.py
@@ -3,3 +3,38 @@
 # Uma listagem com as pessoas mais pesadas.
 # Uma listagem com as pessoas mais leves.
 
+pessoas = []
+dado = []
+contP = mai = men = 0
+
+while True:
+    dado.append(str(input("Digite o nome: ")).strip().title()) 
+    dado.append(float(input("Digite o peso: ")))
+
+    if len(pessoas) == 0:
+        mai = men = dado[1]
+    else:
+        if dado[1] > mai:
+            mai = dado[1]
+        if dado[1] < men:
+            men = dado[1]
+
+    pessoas.append(dado[:])
+    dado.clear()
+    contP += 1
+
+    next = str(input("Deseja Continuar[Sim/Nao]: ")).strip()[0]
+    if next not in 'SsNn' or next in 'Nn':
+        break
+
+print(f"\nPessoas Cadastradas: {contP}")
+
+print(f"Maior Peso: {mai}KG - ", end='')
+for p in pessoas:
+    if p[1] == mai:
+        print(f"[{p[0]}] ", end='')
+
+print(f"\nMenor Peso: {men}KG - ", end='')
+for p in pessoas:  
+    if p[1] == men:
+        print(f"[{p[0]}] ", end='')
