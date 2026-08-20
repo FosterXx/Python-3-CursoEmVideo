43dbb050102da6bd7d5887bc46185dd040c572d6
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-20 09:31:29 -0300

    RX-85 Finalizado!

--- 14 - Listas em Python/EX-85 - Listas com pares e ímpares.py
+++ 14 - Listas em Python/EX-85 - Listas com pares e ímpares.py
@@ -1,2 +1,16 @@
 # Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, exiba os valores pares e ímpares em ordem crescente.
 
+numeros = [[], []]
+
+for i in range(1,8):
+    num = int(input(f"DIgite o {i}° número: "))
+
+    if num % 2 == 0:
+        numeros[0].append(num)
+    else:
+        numeros[1].append(num)
+
+numeros[0].sort()
+numeros[1].sort()
+
+print(f"\nPares: {numeros[0]}\nImpares: {numeros[1]}\n")
