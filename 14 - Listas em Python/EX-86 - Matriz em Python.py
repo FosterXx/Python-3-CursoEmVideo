c86f388229f8c4ba7eae91d2574c9656714a1b84
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-20 16:17:18 -0300

    EX-86 Finalizado!

--- 14 - Listas em Python/EX-86 - Matriz em Python.py
+++ 14 - Listas em Python/EX-86 - Matriz em Python.py
@@ -1,1 +1,20 @@
 # Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+
+matriz = []
+linha = []
+
+for l in range(0,3):
+    for c in range(0,3):
+        linha.append(int(input(f"Digite o valor da Linha[{l}] - Coluna [{c}]: ")))
+
+    matriz.append(linha[:])
+    linha.clear()
+
+print("")
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f"[{matriz[l][c]:^5}]", end='')
+    print()
+    
+print("\nFIM\n")
