ecbf98cba5976309f8fff214cf8216c7f89d12e6
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-24 07:42:45 -0300

    EX-90 Finalizado!

--- 15 - Dicionários em Python/EX-90 - Dicionário em Python.py
+++ 15 - Dicionários em Python/EX-90 - Dicionário em Python.py
@@ -1,2 +1,14 @@
 # Leia nome, média e situação (Aprovado/Reprovado) de um aluno e armazene em um dicionário, exibindo o resultado final.
 
+alunos = {}
+
+alunos['nome'] = str(input("Nome: "))
+alunos['media'] = float(input(f"Média de {alunos['nome']}: "))
+
+if alunos['media'] >= 7:
+    alunos['situacao'] = 'Aprovado'
+else:
+    alunos['situacao'] = 'Reprovado'
+
+
+print(f"\nNome: {alunos['nome']}\nMédia: {alunos['media']}\nSituação: {alunos['situacao']}\n")
