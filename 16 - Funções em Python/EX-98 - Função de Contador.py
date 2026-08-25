ac011d16ce64709f7bed4892231c1b4f9bc0e333
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-25 16:19:59 -0300

    EX-98 Finalizado!

--- 16 - Funções em Python/EX-98 - Função de Contador.py
+++ 16 - Funções em Python/EX-98 - Função de Contador.py
@@ -3,3 +3,41 @@
 # De 10 até 0, de 2 em 2.
 # Uma contagem personalizada.
 
+from time import sleep
+
+def contador(inicio, fim, passo):
+    print("~"*25)
+    print(f"Contagem de {inicio} até {fim} de {passo} em {passo}")
+
+    if passo < 0:
+        passo *= -1
+    if passo == 0:
+        passo = 1
+
+    if inicio < fim:
+        cont = inicio
+        while cont <= fim:
+            print(f"{cont} ", end='', flush=True)
+            sleep(.5)
+            cont += passo
+        print("FIM")
+    else:
+        cont = inicio
+        while cont >= fim:
+            print(f"{cont} ", end='', flush=True)
+            sleep(.5)
+            cont -= passo
+        print("FIM")
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+
+print("~"*25)
+
+print("\nAgora é sua vez, defina os parametros para uma contagem personalizada:")
+inicio = int(input("Inicio: "))
+fim = int(input("Fim: "))
+passo = int(input("Passo: "))
+
+contador(inicio,fim,passo)
