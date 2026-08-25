760d71f1f47611209658f687104177ec20c12320
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-25 08:08:02 -0300

    EX-95 Finalizado!

--- 15 - Dicionários em Python/EX-95 - Aprimorando os Dicionários.py
+++ 15 - Dicionários em Python/EX-95 - Aprimorando os Dicionários.py
@@ -1,12 +1,14 @@
-# Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+# Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de registro do aproveitamento de cada jogador.
 
 jogador = {}
-jogadores = []
 gols = []
-totalG = 0
+
+jogadores = []
 
 while True:
     jogador.clear()
+    gols.clear()
+
     jogador['nome'] = str(input("Nome: ")).strip().title()
 
     partidas = int(input(f"Quantas partidas {jogador['nome']} jogou: "))
@@ -19,21 +21,45 @@ while True:
     jogadores.append(jogador.copy())
 
     while True:
-        next = str(input("Deseja Continuar[Sim/Não]: "))
-        
+        next = str(input("Deseja Continuar[Sim/Não]: ")).strip().upper()[0]
+        if next in 'SsNn':
+            break
+    if next in 'Nn':
+        break
 
-print("="*40)
-print(jogador)
-print("="*40)
+print()
+print("="*45)
 
-for k, v in jogador.items():
-    print(f"O campo {k} tem valor {v}")
+print(f"{'N. Registro':<15}{'Jogador':^15}{'Total de Gol':>15}")
 
-print("="*40)
+for k, v in enumerate (jogadores):
+    print("_"*45)
+    print(f"{k:^11}{v['nome']:^23}{v['total']:>6}")
 
-print(f"O jogador {jogador['nome']} jogou {len(jogador['gols'])} partidas:")
+print("="*45)
 
-for i, v in enumerate(jogador['gols']):
-    print(f"  - Partida {i+1} - {v} gols")
+while True:
+    registro = int(input("\nDigite o registro do jogador que deseja ver os detalhes (9999 PARA ENCERRAR): "))
 
-print(f"Total de {jogador['total']} gols!")
+    if registro == 9999:
+        break
+
+    if registro < len(jogadores):
+
+        print(f"\nLEVANTAMENTO DO JOGADOR {jogadores[registro]['nome']}")
+        print(f"O jogador {jogadores[registro]['nome']} jogou {len(jogadores[registro]['gols'])} partidas:")
+
+        for i, v in enumerate(jogadores[registro]['gols']):
+            print(f"  - Partida {i+1} - {v} gols")
+
+        print(f"Total de {jogadores[registro]['total']} gols!")
+
+    else:
+        print(f"\nNão existe jogador com o registro {registro}!")
+
+    while True:
+        next = str(input("\nDeseja Continuar[Sim/Não]: ")).strip().upper()[0]
+        if next in 'SsNn':
+            break
+    if next in 'Nn':
+        break
