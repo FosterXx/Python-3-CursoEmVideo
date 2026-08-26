42f8de7ba6c72d2e89b03505f2ee58e0634ec8ed
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-26 16:08:31 -0300

    Fazendo EX-101;

--- 16 - Funções em Python/EX-101 - Funções para votação.py
+++ 16 - Funções em Python/EX-101 - Funções para votação.py
@@ -1,2 +1,17 @@
 # Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 
+from datetime import date
+
+def voto(anoNasc):
+    anoAtual = date.today().year
+
+    idade = anoAtual - anoNasc
+
+    return
+
+
+
+# Programa Principal;
+ano = int(input("Digite seu ano de nascimento: "))
+
+voto(ano)
