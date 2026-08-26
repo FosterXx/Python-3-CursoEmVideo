ce61b25b7a0a1726d6388a687d8a2e886ce32a55
Gabriel Ignácio <gabrielhen.ignacio@gmail.com>
2026-08-26 13:26:31 -0300

    EX-99 Finalizado!

--- 16 - Funções em Python/EX-99 - Função que descobre o maior.py
+++ 16 - Funções em Python/EX-99 - Função que descobre o maior.py
@@ -1,2 +1,22 @@
 # Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
+def maior(*num):
+    print(f"O maior valor é o número {max(*num)}!")
+
+
+numList = []
+
+while True:
+    num = int(input("Digite um número: "))
+
+    numList.append(num)
+
+    while True:
+        next = str(input("Deseja Continuar[Sim/Não]: ")).strip().upper()[0]
+        if next in 'NnSs':
+            break
+    if next in 'Nn':
+        break
+print()
+
+maior(numList)
